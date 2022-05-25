d30a908e502c05c5e1487239b45dc0bbef222bfd
Braindead3 <malosay21@gmail.com>
2022-05-25 20:07:55 +0300

    fix

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import CategoryCreateView,ClothListCreateView,ClothUpdateView
+from .views import CategoryCreateView,ClothListCreateView,ClothUpdateDestroyView
 
 urlpatterns = [
     path('cloth/', ClothListCreateView.as_view()),
-    path('cloth/<int:pk>/', ClothUpdateView.as_view()),
+    path('cloth/<int:pk>/', ClothUpdateDestroyView.as_view()),
     path('category/', CategoryCreateView.as_view()),
 ]
